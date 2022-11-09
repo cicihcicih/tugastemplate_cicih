ac660e06b2b4c5b2915e66d3b5ab185fac017528
cicihcicih <ccicih950@gmail.com>
2022-11-09 16:01:14 +0700

    first commit

---
+++ database/migrations/2022_11_09_074447_create_kelas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKelasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kelas', function (Blueprint $table) {
+            $table->id();
+            //nama_kelas ....create nama as varchar data type
+            $table->string('nama_kelas');
+            //jurusan .... create jurusan as varchar data type 
+            $table->string('jurusan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kelas');
+    }
+}
---
+++ resources/views/account/show.blade.php
@@ -0,0 +1,5 @@
+@extends ('template.master')
+
+@section('judul')
+<h1>INI HALAMAN SHOW</h1>
+@endsection
